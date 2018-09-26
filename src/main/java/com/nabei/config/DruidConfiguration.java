23101cbe63c981668803fb1281c845b18956e2e1
huangxiaosen <hxs_82314976@126.com>
2018-09-26 16:36:55 +0800

    初始化

---
+++ src/main/java/com/nabei/config/DruidConfiguration.java
@@ -0,0 +1,42 @@
+package com.nabei.config;
+
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: huangxiaosen
+ * @Date: 2018/9/25 00:49
+ * @Description:
+ */
+@Configuration
+public class DruidConfiguration {
+
+
+    @Bean
+    public ServletRegistrationBean ruidStatViewServle() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        servletRegistrationBean.addInitParameter("allow", "58.62.228.54");
+        //servletRegistrationBean.addInitParameter("deny", "192.168.1.73");
+        servletRegistrationBean.addInitParameter("loginUsername", "root");
+        servletRegistrationBean.addInitParameter("loginPassword", "nb@2018");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean druidStatFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+
+    }
+}
---
+++ src/main/java/com/nabei/config/MybatisPlusConfig.java
@@ -0,0 +1,33 @@
+package com.nabei.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: huangxiaosen
+ * @Date: 2018/9/24 23:12
+ * @Description: Mybatis-plus 插件
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 乐观锁插件
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor(){
+        return new OptimisticLockerInterceptor();
+    }
+
+}
